7117e66d046b69fd63f94e0b8d6865b99da003e7
dosnet <dosnet2200@gmail.com>
2024-06-23 18:21:38 +0300

    Refactor project controller for better readability

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const indexRouter = require('./routes/index');
+
+const projects = require('./routes/projects');
+const connectDB = require('./db/connect');
 
 require('dotenv').config();
 
@@ -8,12 +10,18 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.use(express.static('public'));
 
-app.use('/', indexRouter);
+//app.use(express.static('./public'));
+app.use(express.json());
+app.use('/', projects);
 
-app.listen(PORT, (error) => {
-    if (!error) {
-        console.log(`Server is running on port ${PORT}`);
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
+    } catch (error) {
+        console.log(error);
     }
-});
+};
+
+start();
